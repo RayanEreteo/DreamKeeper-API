e52117d97b3a3a0571104032aafe7bfd91fd9a98
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2024-12-10 16:12:24 +0100

    typo et retrait TODO

--- modules/inputsValidator.ts
+++ modules/inputsValidator.ts
@@ -4,15 +4,13 @@ interface ValidationResult {
 }
 
 /**
- * Verifie si les inputs de formulaire d'authentification sont correctes.
+ * Vérifie si les inputs de formulaire d'authentification sont correctes.
  * 
  * @param email 
  * @param password
  * @param dreamMemory
  */
 
-
-// TODO : Ajouter la verification de dreamMemory
 export function areInputsValid(email: string, password: string, dreamMemory?: string): ValidationResult {
   if (email == "" || password == "" || email == null || password == null || dreamMemory == null) return {success: false, message: "Please fill all the fields."}
   if (!email.match("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$")) return { success: false, message: "Invalid email, please try again."}
