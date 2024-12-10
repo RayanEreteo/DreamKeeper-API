8c0485b9d52e3bfdb6805ae912c7fd971f42a5b0
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2024-12-10 16:30:34 +0100

    ajout de check taille minimum

--- modules/inputsValidator.ts
+++ modules/inputsValidator.ts
@@ -14,7 +14,7 @@ interface ValidationResult {
 export function areInputsValid(email: string, password: string, dreamMemory?: string): ValidationResult {
   if (email == "" || password == "" || email == null || password == null || dreamMemory == null) return {success: false, message: "Please fill all the fields."}
   if (!email.match("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$")) return { success: false, message: "Invalid email, please try again."}
-  if (password.length > 40 || email.length > 40) return {success: false,message: "Please respect the inputs max length.",}
+  if (password.length < 8 || password.length > 40 || email.length < 2 || email.length > 40) return {success: false,message: "Please respect the inputs length.",}
 
   return {success: true}
 }
