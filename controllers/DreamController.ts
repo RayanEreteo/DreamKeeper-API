ea4ca1ae4e51d19b2dd68f526dafa5e77bbbb79a
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2024-12-13 21:26:09 +0100

    Ajout de rêve avec placeholder

--- controllers/DreamController.ts
+++ controllers/DreamController.ts
@@ -1,3 +1,22 @@
-export async function addDream(){
+import { Request, Response } from "express";
+import { connect } from "../modules/dbConn"
 
+export async function addDream(req: Request, res: Response): Promise<any> {
+    try {
+        const conn = await connect()
+
+        await conn.execute("INSERT INTO dream(dreamName, dreamContent, isLucid) VALUES(?, ?, ?)", [
+            "a forest",
+            "i was in a forest",
+            false
+        ])
+
+        console.log("Dream inserted");
+        return res.send("test")
+
+    } catch (error: any) {
+        res.send("Erreur : ")
+        console.log(error);
+        
+    }
 }
