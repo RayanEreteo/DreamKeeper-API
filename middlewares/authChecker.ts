c244892053e78a19caf5763ec8632203d5278fb3
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2024-12-14 18:01:50 +0100

    meilleur code

--- middlewares/authChecker.ts
+++ middlewares/authChecker.ts
@@ -1,12 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
-export function authChecker(req: Request, res: Response, next: NextFunction) {
+export function authChecker(req: Request, res: Response, next: NextFunction): any {
   const token = req.cookies.authToken;
+  const payload = verify(token, process.env.JWT_SECRET!);
+  
+  if (!payload) return res.json({success: false, message: "Invalid token, please login"})
 
-  const ok = verify(token, process.env.JWT_SECRET!);
-
-  if (!ok) return res.json({success: false, message: "Invalid token, please login"})
-
+  req.body.payload = payload
   return next()
 }
