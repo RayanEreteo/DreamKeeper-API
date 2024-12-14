b14957591b9edcc467783a80770a07746a9dfc1b
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2024-12-14 18:01:42 +0100

    placeholder del.

--- controllers/DreamController.ts
+++ controllers/DreamController.ts
@@ -2,13 +2,17 @@ import { Request, Response } from "express";
 import { connect } from "../modules/dbConn"
 
 export async function addDream(req: Request, res: Response): Promise<any> {
+    const {dreamName, dreamContent, isLucid} = req.body
+    const email = req.body.payload.email
+
     try {
         const conn = await connect()
-
-        await conn.execute("INSERT INTO dream(dreamName, dreamContent, isLucid) VALUES(?, ?, ?)", [
-            "a forest",
-            "i was in a forest",
-            false
+        
+        await conn.execute("INSERT INTO dream(author, dreamName, dreamContent, isLucid) VALUES(?, ?, ?, ?)", [
+            email,
+            dreamName,
+            dreamContent,
+            isLucid
         ])
 
         console.log("Dream inserted");
