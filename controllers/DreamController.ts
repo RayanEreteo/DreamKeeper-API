c3bc2392308145ea694327c1ac08fd3b89c63655
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2024-12-15 15:18:32 +0100

    Implementation input validator

--- controllers/DreamController.ts
+++ controllers/DreamController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { connect } from "../modules/dbConn"
+import { areDreamInputsValid } from "../modules/inputsValidator";
 
 export async function addDream(req: Request, res: Response): Promise<any> {
     const {dreamName, dreamContent, isLucid} = req.body
@@ -8,6 +9,11 @@ export async function addDream(req: Request, res: Response): Promise<any> {
     try {
         const conn = await connect()
         
+        const inputsValidResponseObject = areDreamInputsValid(dreamName, dreamContent, isLucid);
+        if (inputsValidResponseObject.success === false) {
+          return res.status(400).json(inputsValidResponseObject); // 400 for bad request (validation failed)
+        }
+
         await conn.execute("INSERT INTO dream(author, dreamName, dreamContent, isLucid) VALUES(?, ?, ?, ?)", [
             email,
             dreamName,
